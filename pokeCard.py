f70a3ca25b981ad76460acbbd333be9ef7544224
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-28 09:44:55 +0530

    Generating Random Card

--- pokeCard.py
+++ pokeCard.py
@@ -14,5 +14,8 @@ response = requests.get(url)
 # Validating Response from API
 if response:
 	cards = response.json()["cards"]
+	# Generating Random Card
+	rand = random.randrange(0, len(cards), 1)
+	image = cards[rand]["imageUrlHiRes"]
 else:
 	print("Please Enter a valid Pokemon name")

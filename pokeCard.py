312174870ab4634889f9857a3e0a1fc98be07cbf
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-28 09:44:34 +0530

    Validating Response from API

--- pokeCard.py
+++ pokeCard.py
@@ -10,3 +10,9 @@ pokemon = input("Enter the Pokemon : ")
 # Getting response from Pokemontcg API
 url = "https://api.pokemontcg.io/v1/cards?name=" + pokemon
 response = requests.get(url)
+
+# Validating Response from API
+if response:
+	cards = response.json()["cards"]
+else:
+	print("Please Enter a valid Pokemon name")
